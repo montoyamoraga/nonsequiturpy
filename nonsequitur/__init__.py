d1ee7989d8de91829be7f72b5fec116b625c9e82
montoyamoraga <montoyamoraga@gmail.com>
2017-04-23 18:12:02 -0400

    add ted talks

--- nonsequitur/__init__.py
+++ nonsequitur/__init__.py
@@ -1,3 +1,14 @@
+#nonsequitur.py module
+#for generation of non sequitur text
+#by aaron montoya-moraga
+#part of my nyu itp thesis
+#and also final project for class
+#reading and writing electronic text
+#by professor allison parrish
+#april 2017
+#v0.0.2
+
+#import modules
 import os
 from os import system
 import time
@@ -5,7 +16,18 @@ import string
 import random
 import urllib
 
+import spacy
 
-def joke():
-    print "this is a premise"
-    print "this is a punchline"
+#define function for jokes
+def joke(waitTime=5):
+
+    #define premise and punchline
+    premise = "this is a premise"
+    punchline = "this is a punchline"
+
+    #print premise
+    print premise
+    # wait
+    time.sleep(waitTime)
+    #print punchline
+    print punchline
